70f5bae961250c6c616ab4d0818849342996b008
Emanuel <emanuel.morille@proton.me>
2023-04-19 19:42:21 +0200

    Documentation pour les classes Mine

--- src/mine.py
+++ src/mine.py
@@ -3,47 +3,29 @@ from src.base import Object
 
 class Mine(Object):
     """
-    class Mine:
-
-    - position (in px): x,y (type -> int)
-    - shape (in px) : height,width (type -> int)
-    - status (alive/destroyed)  : status (type -> bool)
-
+    Mines doesn't move, but they still kill the player
     """
 
-    def __init__(
-        self, width: int = 0, height: int = 0, x: int = 0, y: int = 0, points: int = 0
-    ):
-        """
-        initialisation
-        """
-        super().__init__(width, height, x, y)
+    def __init__(self, width: int, height: int, x: int, y: int, points: int):
+        super().__init__(width, height, x, y, 0, 0)
         self.points = points
 
 
 class PhotonMine(Mine):
     """
-    class Photon_Mine:
-
+    The Photon Mine is half a Vapor Mine
     """
 
-    def __init__(self, x: int = 0, y: int = 0):
-        """
-        initialisation
-        """
+    def __init__(self, x: int, y: int):
         super().__init__(16, 16, x, y, 350)
         self.set_image("PhotonMine.png")
 
 
 class VaporMine(Mine):
     """
-    class Vapor_Mine:
-
+    The Vapor Mine is twice a Photon Mine
     """
 
-    def __init__(self, x: int = 0, y: int = 0):
-        """
-        initialisation
-        """
+    def __init__(self, x: int, y: int):
         super().__init__(24, 24, x, y, 500)
         self.set_image("VaporMine2.png")
