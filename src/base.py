243357e1439a3bde277a8471f80f84b78ee580ad
Emanuel <emanuel.morille@proton.me>
2023-04-19 19:24:23 +0200

    Documentation pour les classes Object et Background

--- src/base.py
+++ src/base.py
@@ -4,14 +4,19 @@ from src.const import WIN_WIDTH, WIN_HEIGHT, BLACK, WHITE, RED
 
 
 class Object:
+    """
+    The most basic class, an Object represents anything in pygame
+    It can move, and draw itself on the game
+    """
+
     def __init__(
         self,
-        width: int = 0,
-        height: int = 0,
-        x: int = 0,
-        y: int = 0,
-        dx: int = 0,
-        dy: int = 0,
+        width: int,
+        height: int,
+        x: int,
+        y: int,
+        dx: int,
+        dy: int,
     ):
         self.set_size(width, height)
         self.set_position(x, y)
@@ -19,34 +24,58 @@ class Object:
         self.set_image()
 
     def set_size(self, width: int, height: int):
+        """
+        Set the size of the object, width and height should be positive
+        """
         self.width = width
         self.height = height
 
     def set_position(self, x: int, y: int):
+        """
+        Set the position of the object, x and y should be positive
+        """
         self.x = x
         self.y = y
 
     def set_direction(self, dx: int, dy: int):
+        """
+        Set the direction of the object
+        """
         self.dx = dx
         self.dy = dy
 
     def set_image(self, filename: str = None, surface: pygame.Surface = None):
+        """
+        Set the image of the object,
+        it can be retrieved from a file or given directly as a surface
+        """
         if not filename is None:
             self.image = pygame.image.load(f"src/assets/{filename}")
         elif not surface is None:
             self.image = surface
         else:
+            # Default case for development
             self.image = pygame.Surface((self.width, self.height))
             self.image.fill(RED)
 
     def draw(self, surface: pygame.Surface):
+        """
+        Draw the object on the surface
+        """
         surface.blit(self.image, (self.x, self.y))
 
     def move(self, dt: float):
+        """
+        Move the object
+        We use dt to make the object move at the same speed on any computer
+        """
         self.x += self.dx * dt
         self.y += self.dy * dt
 
     def collide(self, other) -> bool:
+        """
+        Check for collision with any other object
+        """
         return (
             self.x <= other.x + other.width
             and other.x <= self.x + self.width
@@ -56,6 +85,11 @@ class Object:
 
 
 class Background:
+    """
+    The Background of the game, every other object will be displayed over it
+    There are stars moving, so beautiful
+    """
+
     def __init__(self):
         self.image = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
         self.stars = [
@@ -68,6 +102,9 @@ class Background:
         ]
 
     def move(self, dt: int):
+        """
+        Move stars on the background
+        """
         self.image.fill(BLACK)
         for star in self.stars:
             pygame.draw.line(self.image, WHITE, (star[0], star[1]), (star[0], star[1]))
