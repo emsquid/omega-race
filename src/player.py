3933308e2b31a4b7a9b575793d2306e1ad03a3cd
Elise <elise.cocault@gmail.com>
2023-04-19 16:22:58 +0200

    Création classe Player

---
+++ src/player.py
@@ -0,0 +1,14 @@
+from base import Object
+import pygame
+
+class Player(Object):
+    """
+    class Player:
+
+    -
+    -
+    """
+
+    def __init__(self):
+        super().__init__(32,32,500,200,1,1)
+        
