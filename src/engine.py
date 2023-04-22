a098f40c392b0c73887fc6a4222fd7513184ce76
Emanuel <emanuel.morille@proton.me>
2023-04-22 15:34:26 +0200

    Ajout de la classe Engine pour gérer la logique de jeu

---
+++ src/engine.py
@@ -0,0 +1,104 @@
+import pygame
+from random import randrange
+from src.base import Object, Explosion, Text
+from src.graphics import ForceField
+from src.sprites import Player, DroidShip, CommandShip, DeathShip
+
+
+class Engine:
+    def __init__(self):
+        self.player = Player()
+        self.enemies = (
+            [DroidShip(randrange(200, 800), randrange(550, 750)) for i in range(4)]
+            + [CommandShip(randrange(200, 800), randrange(550, 750)) for i in range(2)]
+            + [DeathShip(randrange(200, 800), randrange(550, 750)) for i in range(1)]
+        )
+        self.mines = []
+        self.player_lasers = []
+        self.enemies_lasers = []
+        self.explosions = []
+
+        self.force_field = ForceField()
+
+    def get_objects(self) -> list[Object]:
+        score_text = Text("SCORE", 330, 330)
+        score = Text(str(self.player.score), 330, 350)
+        return [
+            self.player,
+            *self.enemies,
+            *self.mines,
+            *self.player_lasers,
+            *self.enemies_lasers,
+            *self.explosions,
+            self.force_field,
+            score_text,
+            score,
+        ]
+
+    def handle_events(self, events: list[pygame.event.Event]):
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.player.rotating = "left"
+                elif event.key == pygame.K_RIGHT:
+                    self.player.rotating = "right"
+                elif event.key == pygame.K_UP and self.player.can_thrust():
+                    self.player.thrust()
+                elif event.key == pygame.K_SPACE and self.player.can_shoot():
+                    self.player.shoot(self.player_lasers)
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT and self.player.rotating == "left":
+                    self.player.rotating = ""
+                elif event.key == pygame.K_RIGHT and self.player.rotating == "right":
+                    self.player.rotating = ""
+
+    def update(self, dt: int):
+        # TODO: Improve that
+        self.player.move(dt)
+
+        for enemy in self.enemies:
+            enemy.move(dt)
+            if self.player.collide(enemy):
+                self.player.killed()
+                self.explosions.append(Explosion(enemy.x, enemy.y))
+                self.enemies.remove(enemy)
+            if isinstance(enemy, CommandShip) and enemy.can_shoot():
+                enemy.shoot(self.player, self.enemies_lasers)
+            if isinstance(enemy, (CommandShip, DeathShip)) and enemy.can_drop():
+                enemy.drop_mine(self.mines)
+
+        for mine in self.mines:
+            if self.player.collide(mine):
+                self.player.killed()
+                self.explosions.append(Explosion(mine.x, mine.y))
+                self.mines.remove(mine)
+
+        for laser in self.player_lasers:
+            laser.move(dt)
+            for enemy in self.enemies + self.mines:
+                if laser.collide(enemy):
+                    self.player.kill(enemy)
+                    self.explosions.append(Explosion(enemy.x, enemy.y))
+                    if enemy in self.enemies:
+                        self.enemies.remove(enemy)
+                    else:
+                        self.mines.remove(enemy)
+                    self.player_lasers.remove(laser)
+                    break
+
+        for laser in self.enemies_lasers:
+            laser.move(dt)
+            if laser.collide(self.player):
+                self.player.killed()
+                self.explosions.append(Explosion(self.player.x, self.player.y))
+                self.enemies_lasers.remove(laser)
+
+        for explosion in self.explosions:
+            if explosion.done:
+                self.explosions.remove(explosion)
+
+        self.force_field.bounce([self.player, *self.enemies])
+        self.force_field.crash(self.player_lasers)
+        self.force_field.crash(self.enemies_lasers)
--- src/game.py
+++ src/game.py
@@ -1,15 +1,9 @@
 import os
 import pygame
-from random import randrange
-from src.base import Object
-from src.graphics import Background, ForceField
-from src.sprites import (
-    Player,
-    DroidShip,
-    CommandShip,
-    DeathShip,
-)
-from src.const import WIN_WIDTH, WIN_HEIGHT, WHITE
+from src.base import Object, Text
+from src.graphics import Background
+from src.engine import Engine
+from src.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
@@ -27,39 +21,9 @@ class Game:
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT), pygame.RESIZABLE)
         self.clock = pygame.time.Clock()
         self.background = Background()
-        self.force_field = ForceField()
-
-        self.player = Player()
-        self.enemies = (
-            [DroidShip(randrange(200, 800), randrange(550, 750)) for i in range(4)]
-            + [CommandShip(randrange(200, 800), randrange(550, 750)) for i in range(2)]
-            + [DeathShip(randrange(200, 800), randrange(550, 750)) for i in range(1)]
-        )
-        self.mines = []
-        self.player_lasers = []
-        self.enemies_lasers = []
+        self.engine = Engine()
 
         self.running = False
-
-    def handle_events(self):
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                self.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.player.rotating = "left"
-                elif event.key == pygame.K_RIGHT:
-                    self.player.rotating = "right"
-                elif event.key == pygame.K_UP and self.player.can_thrust():
-                    self.player.thrust()
-                elif event.key == pygame.K_SPACE and self.player.can_shoot():
-                    self.player.shoot(self.player_lasers)
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT and self.player.rotating == "left":
-                    self.player.rotating = ""
-                elif event.key == pygame.K_RIGHT and self.player.rotating == "right":
-                    self.player.rotating = ""
 
     def draw(self, *objects: tuple[Object]):
         """
@@ -70,77 +34,32 @@ class Game:
         game = pygame.transform.scale(self.background.image, self.screen.get_size())
         self.screen.blit(game, (0, 0))
 
+    def handle_events(self):
+        """
+        Handle user inputs
+        """
+        events = pygame.event.get()
+        if self.running:
+            self.engine.handle_events(events)
+
     def update(self):
         """
         Update the situation of all objects
         """
         dt = self.clock.tick(60)
-
-        self.player.move(dt)
-        self.force_field.bounce(self.player)
-
-        for enemy in self.enemies:
-            enemy.move(dt)
-            self.force_field.bounce(enemy)
-            if self.player.collide(enemy):
-                self.player.killed()
-                enemy.explode()
-                self.enemies.remove(enemy)
-            if isinstance(enemy, CommandShip) and enemy.can_shoot():
-                enemy.shoot(self.player, self.enemies_lasers)
-            if isinstance(enemy, (CommandShip, DeathShip)) and enemy.can_drop():
-                enemy.drop_mine(self.mines)
-
-        for mine in self.mines:
-            if self.player.collide(mine):
-                mine.explode()
-                self.mines.remove(mine)
-
-        self.force_field.crash(self.player_lasers)
-        for laser in self.player_lasers:
-            laser.move(dt)
-            for enemy in self.enemies + self.mines:
-                if laser.collide(enemy):
-                    self.player.kill(enemy)
-                    enemy.explode()
-                    if enemy in self.enemies:
-                        self.enemies.remove(enemy)
-                    else:
-                        self.mines.remove(enemy)
-                    self.player_lasers.remove(laser)
-                    break
-
-        self.force_field.crash(self.enemies_lasers)
-        for laser in self.enemies_lasers:
-            laser.move(dt)
-            if laser.collide(self.player):
-                laser.explode()
-                self.enemies_lasers.remove(laser)
-
-        self.background.move(dt)
-
-        font = pygame.font.Font('assets/font1.ttf', 25)
-        score_text = font.render("Score", True, WHITE)
-        score = font.render(f'{self.player.score}',True,WHITE)
-        self.background.image.blit(score_text, (330, 320))
-        self.background.image.blit(score, (330,350))
+        self.background.update(dt)
+        if self.running:
+            self.engine.update(dt)
 
     def run(self):
         """
-        Run the game instance to make it playable
+        Run the game instance
         """
         self.running = True
         while self.running:
             self.handle_events()
             self.update()
-            self.draw(
-                self.force_field,
-                self.player,
-                *self.enemies,
-                *self.mines,
-                *self.player_lasers,
-                *self.enemies_lasers,
-            )
+            self.draw(*self.engine.get_objects())
             pygame.display.update()
 
     def exit(self):
