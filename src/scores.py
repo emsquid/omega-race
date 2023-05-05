e693c4dcd2bf349488e5a304ecbd00145f18a189
Emanuel <emanuel.morille@proton.me>
2023-05-06 00:49:25 +0200

    Correction de petits bugs de BD

--- src/scores.py
+++ src/scores.py
@@ -9,6 +9,7 @@ from src.settings import Settings
 from src.const import RED
 
 
+# TODO: Remove prints when good
 class Scores:
     """
     The scores of the game
@@ -22,33 +23,15 @@ class Scores:
 
         self.title = Text("Scores", 500, 150, size=90)
         self.names = [
-            Text(
-                f"{i+1}. {self.array[i]['name'] if i<len(self.array) else '-----'}",
-                150 - i,
-                240 + i * 40,
-                anchor="topleft",
-            )
+            Text(f"{i+1}. -----", 150 - i, 240 + i * 40, anchor="topleft")
             for i in range(10)
         ]
         self.scores = [
-            Text(
-                f"{self.array[i]['score'] if i<len(self.array) else '-----'}",
-                550,
-                240 + i * 40,
-                anchor="topright",
-            )
-            for i in range(10)
+            Text("-----", 550, 240 + i * 40, anchor="topright") for i in range(10)
         ]
         self.levels = [
-            Text(
-                f"{self.array[i]['level'] if i<len(self.array) else '--'}",
-                800,
-                240 + i * 40,
-                anchor="topright",
-            )
-            for i in range(10)
+            Text("--", 800, 240 + i * 40, anchor="topright") for i in range(10)
         ]
-
         self.home = Text("HOME", 800, 700, RED, 40)
 
     def get_objects(self) -> tuple[Object]:
@@ -75,6 +58,9 @@ class Scores:
         pass
 
     def connect(self):
+        """
+        Try to connect to the remote database
+        """
         if self.connected:
             return
         try:
@@ -89,11 +75,23 @@ class Scores:
             print(e)
 
     def fetch(self):
+        """
+        Try to fetch scores from the remote database
+        """
         if self.connected:
-            self.array = [doc for doc in self.db["Single"].find()]
-            self.array.sort(key=lambda doc: doc["score"], reverse=True)
+            try:
+                self.array = [doc for doc in self.db["Single"].find()]
+                self.array.sort(key=lambda doc: doc["score"], reverse=True)
+            except Exception as e:
+                self.connected = False
+                print(e)
 
     def insert(self, doc: dict):
+        """
+        Try to insert a document in the remote database
+
+        :param doc: dict, The document to insert
+        """
         if self.connected:
             try:
                 self.db["Single"].insert_one(doc)
@@ -102,12 +100,24 @@ class Scores:
                 print(e)
 
     def add_score(self, name: str, score: int, level: int):
+        """
+        Add a score to the database locally (and try remotely)
+
+        :param name: str, The name of the user who made the score
+        :param score: int, The score made
+        :param level: int, The level reached
+        """
         if len(self.array) < 10 or score > self.array[9]["score"]:
             doc = {"name": name, "score": score, "level": level}
             self.array.append(doc)
             self.array.sort(key=lambda doc: doc["score"], reverse=True)
 
-            Thread(target=self.insert, args=(doc)).start()
+            Thread(target=self.insert, args=[doc]).start()
 
     def highscore(self) -> int:
+        """
+        Get the highscore from the database
+
+        :return: int, The highscore
+        """
         return self.array[0]["score"] if len(self.array) > 0 else 0
