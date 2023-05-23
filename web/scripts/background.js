6e855bcd1ce32ecd3846a1d3d41de7d9704d137f
Emanuel <emanuel.morille@proton.me>
2023-05-23 22:22:23 +0200

    Ajout du fond d'écran étoilé

---
+++ web/scripts/background.js
@@ -0,0 +1,37 @@
+const random = (min, max) => {
+    return Math.random() * (max - min) + min
+}
+
+const createStar = () => {
+    const star = document.createElement("div")
+    document.body.appendChild(star)
+    star.classList.add("star")
+    star.style.zIndex = -1
+    star.style.width = "1px"
+    star.style.height = "1px"
+    star.style.position = "fixed"
+    star.style.backgroundColor = "white"
+    return { div: star, x: random(0, 100), y: random(0, 100), speed: random(0, 0.05) }
+}
+
+const updateStar = (star) => {
+    star.y -= star.speed
+    if (star.y < 0) {
+        star.y = 100
+        star.speed = random(0, 0.05)
+    }
+    star.div.style.left = `${star.x}%`
+    star.div.style.top = `${star.y}%`
+}
+
+const stars = []
+
+for (let i = 0; i < 128; i++) {
+    stars.push(createStar())
+}
+
+setInterval(() => {
+    for (let i = 0; i < 128; i++) {
+        updateStar(stars[i])
+    }
+}, 0.1)
