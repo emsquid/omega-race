b15ef10a6535a48d169ff8b9093c68d87a62df33
Emanuel <emanuel.morille@proton.me>
2023-05-24 19:23:07 +0200

    Mise en conformité

--- web/scripts/deroulant.js
+++ web/scripts/deroulant.js
@@ -1,15 +1,19 @@
 const object1 = document.getElementById("s1")
 const liste1 = document.getElementsByClassName("s1")
+
 const object1_1 = document.getElementById("s1_1")
 const liste1_1 = document.getElementsByClassName("s1_1")
+
 const object1_1_1 = document.getElementById("s1_1_1")
 const liste1_1_1 = document.getElementsByClassName("s1_1_1")
+
 const object1_1_2 = document.getElementById("s1_1_2")
 const liste1_1_2 = document.getElementsByClassName("s1_1_2")
+
 const object2 = document.getElementById("s2")
 const liste2 = document.getElementsByClassName("s2")
 
-const sous_classe = (obj, liste) => {
+const deroule_sous_classe = (obj, liste) => {
     if (Array("", "none").includes(liste[0].style.display)) {
         for (let i = 0; i < liste.length; i++) {
             liste[i].style.display = "block";
@@ -24,19 +28,18 @@ const sous_classe = (obj, liste) => {
 }
 
 object1.onclick = () => {
-    sous_classe(object1, liste1)
-};
-
+    deroule_sous_classe(object1, liste1)
+}
 object1_1.onclick = () => {
-    sous_classe(object1_1, liste1_1)
-};
+    deroule_sous_classe(object1_1, liste1_1)
+}
 object1_1_1.onclick = () => {
-    sous_classe(object1_1_1, liste1_1_1)
-};
+    deroule_sous_classe(object1_1_1, liste1_1_1)
+}
 object1_1_2.onclick = () => {
-    sous_classe(object1_1_2, liste1_1_2)
-};
+    deroule_sous_classe(object1_1_2, liste1_1_2)
+}
 object2.onclick = () => {
-    sous_classe(object2, liste2)
-};
+    deroule_sous_classe(object2, liste2)
+}
 
